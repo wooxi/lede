b5957cfb1e2094e2f2a4fc5fb2110a44a1bad405
wooxi <59468499+wooxi@users.noreply.github.com>
2022-12-05 15:22:41 +0800

    Update mac80211.sh
    改默认wifi名，加默认密码

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -181,8 +181,9 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.ssid=Lede
+			set wireless.default_radio${devidx}.encryption=psk2
+                        et wireless.default_radio${devidx}.key=13027679883
 EOF
 		uci -q commit wireless
 
